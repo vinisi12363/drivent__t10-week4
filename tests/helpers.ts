f8933ed22c3c1e1d26176de9b1a695e8d0018f75
vinisi12363 <viniciuspv.si@gmail.com>
2023-07-23 20:03:50 -0300

    testing

--- tests/helpers.ts
+++ tests/helpers.ts
@@ -4,22 +4,22 @@ import { User } from '@prisma/client';
 import { createUser } from './factories';
 import { createSession } from './factories/sessions-factory';
 import { prisma } from '@/config';
-
 export async function cleanDb() {
-  const tablenames = await prisma.$queryRaw<
-    Array<{ tablename: string }>
-  >`SELECT tablename FROM pg_tables WHERE schemaname='public'`;
-
-  const tables = tablenames
-    .map(({ tablename }) => tablename)
-    .filter((name) => name !== '_prisma_migrations')
-    .map((name) => `"public"."${name}"`)
-    .join(', ');
-
   try {
-    await prisma.$executeRawUnsafe(`TRUNCATE TABLE ${tables} CASCADE;`);
+    await prisma.booking.deleteMany({});
+    await prisma.address.deleteMany({});
+    await prisma.payment.deleteMany({});
+    await prisma.ticket.deleteMany({});
+    await prisma.enrollment.deleteMany({});
+    await prisma.event.deleteMany({});
+    await prisma.session.deleteMany({});
+    await prisma.user.deleteMany({});
+    await prisma.ticketType.deleteMany({});
+    await prisma.room.deleteMany({});
+    await prisma.hotel.deleteMany({});
   } catch (error) {
-    console.log({ error });
+    console.error('Error cleaning the database:', error);
+    throw error;
   }
 }
 
